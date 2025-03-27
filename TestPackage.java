1cb322747a28fc615b3d8a717eea65c4acca9c4e
sosina14 <sosiayu14@gmail.com>
2025-03-27 21:15:30 +0300

    AP

---
+++ TestPackage.java
@@ -0,0 +1,9 @@
+// Save this file outside "mypackage" or in a different package
+import mypackage.MyClass;  // Importing the class
+
+public class TestPackage {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();  // Creating an object of MyClass
+        obj.displayMessage();
+    }
+}
---
+++ mypackage/MyClass.java
@@ -0,0 +1,7 @@
+package mypackage;  // Correct package declaration
+
+public class MyClass {
+    public void displayMessage() {
+        System.out.println("Hello from MyClass in mypackage!");
+    }
+}
