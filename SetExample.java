1cce6db6d4d58a91262f361ee176c8df39d1e303
sosina14 <sosiayu14@gmail.com>
2025-03-28 22:45:38 +0300

    AP

---
+++ SetExample.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class SetExample {
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<>();
+        
+        // Adding elements
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(10);  // Duplicate (ignored)
+
+        // Printing Set
+        System.out.println("Numbers Set: " + numbers);
+    }
+}
