9dc0e3218625fa37d7f62025fa08ea1e4fd00674
sosina14 <sosiayu14@gmail.com>
2025-03-29 00:14:47 +0300

    AP

--- Lamda.java
+++ Lamda.java
@@ -1,10 +1,6 @@
 public class Lamda {
     public static void main(String[] args) {
-        // Using a lambda expression to create a Runnable
         Runnable r = () -> System.out.println("Running...");
-        // Using lambda expression to create a thread
-        // and start it immediately
-
         new Thread(r).start();
     }
 }
