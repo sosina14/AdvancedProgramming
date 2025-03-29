e728607ad2436561e4fa82e81ef98445ca523af0
sosina14 <sosiayu14@gmail.com>
2025-03-29 11:07:01 +0300

    AP

---
+++ Lamda2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Lamda2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 4, 7, 6, 3);
+
+        Collections.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return b - a; // Descending Order
+            }
+        });
+
+        System.out.println(numbers);
+    }
+}
