7dbe20236371796e5d069499f9e7752ff576be69
sosina14 <sosiayu14@gmail.com>
2025-03-29 11:05:53 +0300

    AP

--- Lamda.java
+++ Lamda.java
@@ -1,6 +1,8 @@
 public class Lamda {
     public static void main(String[] args) {
-        Runnable r = () -> System.out.println("Running...");
+        Runnable r = () -> System.out.println("Lamda exparation in Java 8");
+        Runnable s = () -> System.out.println("Runnable exparation in Java 8");
         new Thread(r).start();
+        new Thread(s).start();
     }
 }
